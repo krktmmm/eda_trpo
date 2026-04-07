29ed1d2364cfdb1d7fcff06513c6d2f6668588d0
krktmmm <kekatya19@gmail.com>
2026-04-07 09:19:53 +0700

    исправила опечатку

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/', http_metod_names=['get', 'post']), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/', http_method_names=['get', 'post']), name='logout'),
     path('register/', include('users.urls')),
     path('', include('places.urls')),
 ]
