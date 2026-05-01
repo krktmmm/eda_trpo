f1001c9c67b628609835da5df9dc0b331ce6f13b
Кривошеина Екатерина <kekatya19@gmail.com>
2026-05-01 20:13:49 +0700

    почистила файлы от всяких повторений и ненужного мусора

--- static/js/edit_profile.js
+++ static/js/edit_profile.js
@@ -1,18 +1,12 @@
-// Предпросмотр и сохранение файла
+// Предпросмотр аватарки
 document.getElementById('avatar-upload').addEventListener('change', function(e) {
     const file = e.target.files[0];
     if (file) {
-        // Предпросмотр
         const reader = new FileReader();
         reader.onload = function(event) {
             document.getElementById('avatar-preview').src = event.target.result;
         };
         reader.readAsDataURL(file);
-        
-        // Передаём файл в скрытое поле формы
-        const dataTransfer = new DataTransfer();
-        dataTransfer.items.add(file);
-        document.getElementById('avatar-field').files = dataTransfer.files;
     }
 });
 
