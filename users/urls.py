d9c1b2a0b70e18a681e38a0d929a4c5beea34ae5
xdxxxxn <vikabarysheva7@gmail.com>
2026-05-07 16:37:53 +0700

    fix: страница редактирования профиля

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('edit/', views.edit_profile, name='edit_profile'),
     path('settings/', views.settings_view, name='settings'),
-    path('upload-avatar/', views.upload_avatar, name='upload_avatar'),
 ]
